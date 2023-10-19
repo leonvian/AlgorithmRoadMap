5bb92655b59fd0b538751aeb5b3bbd3b6c2a49b4
leonvian <leonardo2050@gmail.com>
2023-10-19 13:12:19 -0300

    Trees

--- src/main/java/tree/HeightOfTree.java
+++ src/main/java/tree/HeightOfTree.java
@@ -6,14 +6,7 @@ public class HeightOfTree {
     public static void main(String[] args) {
         TreeNode root = BinaryTreeFactory.createBinaryTree(new int[]{ 1 , 2 , 3 , 4, 5, 6, 7});
         int size = height(root);
-      //  System.out.println("Size " + size);
-
-
-        int test = 805306368;
-        int result = (int)Math.ceil((double) 3 / 4);
-
-        System.out.println("Size " + test);
-
+        System.out.println("Size " + size);
     }
 
 
